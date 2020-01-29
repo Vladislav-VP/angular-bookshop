fdc54d20367500bbdff0006ea2a19bc310e23ed0
Vladislav Parkhomenko <vladislav.parkhomenko@yandex.ua>
2020-01-29 13:56:32 +0200

    Added component for login, registration, shop, cart and admin

--- angular-bookshop/src/app/app.module.ts
+++ angular-bookshop/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { LoginComponent } from './components/login/login.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { CartComponent } from './components/cart/cart.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent,
+    ShopComponent,
+    CartComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
