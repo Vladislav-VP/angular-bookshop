20a076a6c13462d348fb42c4e4b0ac55b2960286
Vladislav Parkhomenko <vladislav.parkhomenko@yandex.ua>
2020-01-29 16:09:39 +0200

    Added entities

---
+++ angular-bookshop/src/app/entities/book.ts
@@ -0,0 +1,13 @@
+export class Book {
+    
+    id: string;
+    title: string;
+    author: string;
+    published: Date;
+    publisher: string;
+    pages: number;
+    description: string;
+    count: number;
+    price: number;
+
+}
