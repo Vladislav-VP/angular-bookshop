4128993f943c586082d727c6235a34f1e3e91afc
Vladislav Parkhomenko <vladislav.parkhomenko@yandex.ua>
2020-01-30 12:38:25 +0200

    Set up routing

--- angular-bookshop/src/app/app-routing.module.ts
+++ angular-bookshop/src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CartComponent } from "./components/cart/cart.component";
+import { ShopComponent } from "./components/shop/shop.component";
+import { AdminComponent } from "./components/admin/admin.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: "cart", component: CartComponent
+  },
+  {
+    path: "shop", component: ShopComponent
+  },
+  {
+    path: "admin", component: AdminComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  //static components = [ CartComponent ];
+ }
